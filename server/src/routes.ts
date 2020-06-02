3cce2a264682d7f2ed41c59bf9d0828deb7cc2b2
mrChernicharo <felipe.chernicharo@gmail.com>
2020-06-02 19:27:26 -0300

    rota GET /items -> listar todos os itens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import knex from './database/connection';
 
 const router = express.Router()
 
-router.get('/', (request, response) => {
-    return response.json({ message: 'hello world'});
+router.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+
+    return response.json(items);
 })
 
 export default router;
