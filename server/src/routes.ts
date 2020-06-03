54a8c28ed8f14e145e8d34e6edc03d4d981ffd16
mrChernicharo <felipe.chernicharo@gmail.com>
2020-06-02 21:24:59 -0300

    inserir point e point_items. Relacionamento N - N

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request, response } from 'express';
 import knex from './database/connection';
 
 const router = express.Router()
@@ -8,6 +8,7 @@ router.get('/items', async (request, response) => {
 
     const serializedItems = items.map(item => {
        return {
+         id: item.id,  
          title: item.title,
          image: item.image,
          image_url: 'http://localhost:3333/uploads/' + item.image,
@@ -17,5 +18,32 @@ router.get('/items', async (request, response) => {
     return response.json(serializedItems);
 })
 
+router.post('/points', async (request, response) => {
+   const { 
+      name, email, whatsapp, longitude, latitude, city, uf, items,
+   } = request.body;
+
+   const ids = await knex('points').insert({
+      name, 
+      image:'fake-img',
+      email,
+      whatsapp, 
+      longitude, 
+      latitude, 
+      city, 
+      uf, 
+   });
+
+   const pointItems = items.map((item_id: number )=> {
+      return {
+         item_id,
+         point_id: ids[0],
+      }
+   })
+
+   await knex('point_items').insert(pointItems);
+
+   return response.json({ success: true })
+});
 
 export default router;
