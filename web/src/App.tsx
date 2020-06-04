d087ee1008dcf95391782cff1b1a922554353154
mrChernicharo <felipe.chernicharo@gmail.com>
2020-06-04 12:14:20 -0300

    primeiro componente, prop e interface

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Header from'./Header';
+
 function App() {
   return (
-    <div className="App">
-        <h1>Hello World</h1>
+    <div id="app">
+      <Header title="Main"/>
+
     </div>
   );
 }
