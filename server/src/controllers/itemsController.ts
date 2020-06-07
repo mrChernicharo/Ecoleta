25071037d079ae589c528934342733621397ddde
mrChernicharo <felipe.chernicharo@gmail.com>
2020-06-07 17:15:58 -0300

    page Points => map + hardCode items + scroll

--- server/src/controllers/itemsController.ts
+++ server/src/controllers/itemsController.ts
@@ -10,7 +10,7 @@ class ItemsController{
              id: item.id,  
              title: item.title,
              image: item.image,
-             image_url: 'http://localhost:3333/uploads/' + item.image,
+             image_url: 'http://192.168.15.4:3333/uploads/' + item.image,
            }
         })
     
