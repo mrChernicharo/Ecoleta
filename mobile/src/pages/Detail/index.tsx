954f91edcfd041e41a11c3067b08dc4f110f898a
mrChernicharo <felipe.chernicharo@gmail.com>
2020-06-07 23:39:15 -0300

    envio de email e whatsapp

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
-import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView, SafeAreaView, Linking } from 'react-native';
 import { Feather as Icon } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
 import Constants from 'expo-constants';
+import * as MailComposer from 'expo-mail-composer';
 import api from '../../services/api';
 
 interface Params{
@@ -17,6 +18,7 @@ interface Data {
     latitude: number;
     longitude: number;
     whatsapp: string;
+    email: string;
     city: string;
     uf: string;
   }, 
@@ -42,6 +44,17 @@ const Detail = () => {
 
   function handleNavigateBack() {
     navigation.navigate('Points');
+  }
+
+  function handleComposeMail() {
+    MailComposer.composeAsync({
+      subject: 'Interesse na coleta de resíduos',
+      recipients: [data.point.email],
+    })
+  }
+
+  function handleWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Interesse na coleta de resíduos`)
   }
 
   if(!data.point) {
@@ -71,11 +84,11 @@ const Detail = () => {
         </View>
       </View>
       <View style={styles.footer}>
-        <RectButton style={styles.button}>
+        <RectButton style={styles.button} onPress={handleWhatsapp}>
           <FontAwesome name="whatsapp" size={20} color="#fff"/>
           <Text style={styles.buttonText}>Whatsapp</Text>
         </RectButton>
-        <RectButton style={styles.button}>
+        <RectButton style={styles.button} onPress={handleComposeMail}>
           <Icon name="mail" size={20} color="#fff"/>
           <Text style={styles.buttonText}>Email</Text>
         </RectButton>
